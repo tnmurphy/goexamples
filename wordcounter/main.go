43c36c4ea4046978040639b1cc66b95ec622cbbf
Timothy Murphy <tnmurphy@gmail.com>
2019-12-01 17:49:46 +0000

    Got the basics working. Only an Insertion sort at the moment and no way to read a file yet.

--- wordcounter/main.go
+++ wordcounter/main.go
@@ -8,7 +8,18 @@ import (
 type Node struct {
     char uint8
     word_count uint
-    edges []*Node
+    children []*Node
+}
+
+func filterToLower(b byte) byte {
+
+        if b >= 'A' && b <= 'Z' {
+            return 'a' + (b - 'A')
+        } else if b >= 'a' && b <= 'z' {
+           return b
+        } else {
+           return ' ' // binary etc converted to space
+        }
 }
 
 
@@ -17,6 +28,8 @@ func (n *Node) get_inserter() func(uint8) {
     inword := false
 
     return func(b uint8) {
+        b = filterToLower(b)
+
         if b == ' ' {
             fmt.Printf("_")
             if (inword) {
@@ -29,36 +42,97 @@ func (n *Node) get_inserter() func(uint8) {
         }
         fmt.Printf("%v",string(b))
         inword = true
-        edges := &(current.edges)
-        for e := 0; e < len(*edges); e++ {
-            if (*edges)[e].char == b {
+        children := &(current.children)
+        for e := range(*children) {
+            if (*children)[e].char == b {
                 fmt.Printf("<")
-                current = (*edges)[e]
+                current = (*children)[e]
                 return
             }
         }
         fmt.Printf("^")
         new_node := &Node{b, 0, nil}
-        current.edges = append(current.edges, new_node)
+        current.children = append(current.children, new_node)
         current = new_node
-        // fmt.Printf("\n+%v\n", current.edges)
+        // fmt.Printf("\n+%v\n", current.children)
 
     }
 
 }
 
-func (n *Node) dump(prefix []uint8, depth int) {
+type Wordcount struct {
+    count uint
+    word string
+}
+
+type Topwords struct {
+    sortedwords []*Wordcount
+    start Node
+}
+
+func NewTopwords(countmax uint) *Topwords {
+        var tw Topwords
+        tw.sortedwords = make([]*Wordcount,0, countmax+1)
+
+        return &tw
+}
+
+
+func (n *Node) preorder_walk(prefix []uint8, action func (n *Node, prefix *[]uint8)) {
         if n.char == 0 { // step over the first node if it's a start node
-            // fmt.Printf("\nSTART depth %v prefix '%v'\n", depth, prefix)
+                // fmt.Printf("\nSTART depth %:v prefix '%v'\n", depth, prefix)
         } else {
             prefix = append(prefix, n.char)
             if n.word_count > 0 {
-                fmt.Printf("=%v: '%v'\n", n.word_count, string(prefix))
+                action(n, &prefix)
             }
         }
-        for e := range(n.edges) {
-            n.edges[e].dump(prefix, depth+1)
+        for e := range(n.children) {
+            n.children[e].preorder_walk(prefix, action)
         }
+}
+
+
+func (tw *Topwords) most_frequent() {
+
+    action := func(n *Node, prefix *[]uint8) {
+        w := new(Wordcount)
+        w.count = n.word_count
+        w.word = string(*prefix)
+
+        max := cap(tw.sortedwords)
+        if len(tw.sortedwords) < max {
+            tw.sortedwords = append(tw.sortedwords, w)
+        }  else {
+            tw.sortedwords[max-1] = w
+            // So what happens to the item at the end of the list of topwords? 
+            // we replaced it without a comparison! Well, we use the end of the
+            // slice as a sort of "entrance/exit hall" and don't intend to show
+            // what's in there finally anyhow - so whatever's "in" there is not
+            // important as it won't make the final "cut" anyhow.
+        }
+
+        // Insertion sort to swap the new word up to its
+        // position.
+        fmt.Printf("---%v\n", len(tw.sortedwords))
+        for f := len(tw.sortedwords)-1; f >= 1 ; f-- {
+            if  tw.sortedwords[f].count > tw.sortedwords[f-1].count {
+                tmp := tw.sortedwords[f]
+                tw.sortedwords[f] = tw.sortedwords[f-1]
+                tw.sortedwords[f-1] = tmp
+            }
+        }
+    }
+
+    tw.start.preorder_walk([]uint8{}, action)
+    tw.sortedwords = tw.sortedwords[:len(tw.sortedwords)-1]
+}
+
+func (n *Node) dump() {
+    action := func(n *Node, prefix *[]uint8) {
+        fmt.Printf("=%v: '%v'\n", n.word_count, string(*prefix))
+    }
+    n.preorder_walk([]uint8{}, action)
 }
 
 
--- wordcounter/main_test.go
+++ wordcounter/main_test.go
@@ -8,7 +8,6 @@ import (
 func TestBasicInput1(t *testing.T) {
     var start Node;
 
-    //test_string := "This is a test with repeated words - words like 'test' and 'This' and 'repeated'."
     test_string := "a aa b"
     fmt.Printf("\n%v\n",test_string)
 
@@ -18,16 +17,14 @@ func TestBasicInput1(t *testing.T) {
     }
     inserter(' ') // ensure the last word is flushed
 
-    fmt.Printf("%v", )
-
-    if len (start.edges) != 2 {
-        t.Errorf("start.edges not 2")
+    if len (start.children) != 2 {
+        t.Errorf("start.children not 2")
     }
-    if start.edges[0].char != 'a' {
-        t.Errorf("first edge is not 'a' but '%v'", string(start.edges[0].char))
+    if start.children[0].char != 'a' {
+        t.Errorf("first edge is not 'a' but '%v'", string(start.children[0].char))
     }
-    if start.edges[1].char != 'b' {
-        t.Errorf("next edge is not 'b' but '%v'", string(start.edges[1].char))
+    if start.children[1].char != 'b' {
+        t.Errorf("next edge is not 'b' but '%v'", string(start.children[1].char))
     }
 
     //expected_dump := "{0 0 [{97 1 [{97 1 []}]} {98 1 []}]}"
@@ -35,7 +32,7 @@ func TestBasicInput1(t *testing.T) {
     //    t.Errorf("struct was '%v' but should be '%v'", dump, expected_dump)
     //}
     fmt.Printf("\n")
-    start.dump([]uint8{}, 0)
+    start.dump()
 
    }
 
@@ -43,7 +40,6 @@ func TestBasicInput1(t *testing.T) {
 func TestBasicInputRepeats(t *testing.T) {
     var start Node;
 
-    //test_string := "This is a test with repeated words - words like 'test' and 'This' and 'repeated'."
     test_string := "a aa b aa b a"
     fmt.Printf("\n%v\n",test_string)
 
@@ -53,16 +49,14 @@ func TestBasicInputRepeats(t *testing.T) {
     }
     inserter(' ') // ensure the last word is flushed
 
-    fmt.Printf("%v", )
-
-    if len (start.edges) != 2 {
-        t.Errorf("start.edges not 2")
+    if len (start.children) != 2 {
+        t.Errorf("start.children not 2")
     }
-    if start.edges[0].char != 'a' {
-        t.Errorf("first edge is not 'a' but '%v'", string(start.edges[0].char))
+    if start.children[0].char != 'a' {
+        t.Errorf("first edge is not 'a' but '%v'", string(start.children[0].char))
     }
-    if start.edges[1].char != 'b' {
-        t.Errorf("next edge is not 'b' but '%v'", string(start.edges[1].char))
+    if start.children[1].char != 'b' {
+        t.Errorf("next edge is not 'b' but '%v'", string(start.children[1].char))
     }
 
     //expected_dump := "{0 0 [{97 1 [{97 1 []}]} {98 1 []}]}"
@@ -70,6 +64,128 @@ func TestBasicInputRepeats(t *testing.T) {
     //    t.Errorf("struct was '%v' but should be '%v'", dump, expected_dump)
     //}
     fmt.Printf("\n")
-    start.dump([]uint8{}, 0)
+    start.dump()
 
    }
+
+
+
+func test_string() string {
+        return `
+**The Project Gutenberg Etext of Moby Dick, by Herman Melville**
+#3 in our series by Herman Melville
+
+This Project Gutenberg version of Moby Dick is based on a combination
+of the etext from the ERIS project at Virginia Tech and another from
+Project Gutenberg's archives, as compared to a public-domain hard copy.
+
+Copyright laws are changing all over the world, be sure to check
+the copyright laws for your country before posting these files!!
+
+Please take a look at the important information in this header.
+We encourage you to keep this file on your own disk, keeping an
+electronic path open for the next readers.  Do not remove this.
+
+
+**Welcome To The World of Free Plain Vanilla Electronic Texts**
+
+**Etexts Readable By Both Humans and By Computers, Since 1971**
+
+*These Etexts Prepared By Hundreds of Volunteers and Donations*
+
+Information on contacting Project Gutenberg to get Etexts, and
+further information is included below.  We need your donations.
+
+
+Title:  Moby Dick; or The Whale
+
+Author:  Herman Melville
+
+June, 2001  [Etext #2701]
+
+**The Project Gutenberg Etext of Moby Dick, by Herman Melville**
+******This file should be named moby10b.txt or moby10b.zip******
+    `
+}
+
+func TestLongInput(t *testing.T) {
+    var start Node;
+    test_string := test_string()
+    fmt.Printf("\n%v\n",test_string)
+
+    var inserter = start.get_inserter()
+    for b := range(test_string) {
+        inserter(test_string[b])
+    }
+    inserter(' ') // ensure the last word is flushed
+
+    if len (start.children) != 23 {
+        t.Errorf("start.children not 23 but %v", len(start.children))
+    }
+    if start.children[0].char != 't' {
+        t.Errorf("first child is not 't' but '%v'", string(start.children[0].char))
+    }
+    if start.children[1].char != 'p' {
+        t.Errorf("next edge is not 'p' but '%v'", string(start.children[1].char))
+    }
+
+    //expected_dump := "{0 0 [{97 1 [{97 1 []}]} {98 1 []}]}"
+    //if dump:=fmt.Sprintf("%v", start); dump != expected_dump {
+    //    t.Errorf("struct was '%v' but should be '%v'", dump, expected_dump)
+    //}
+    fmt.Printf("\n")
+    start.dump()
+
+   }
+
+
+func TestSortedOut(t *testing.T) {
+
+    fmt.Println("\n--------------------------------TestSortedOut\n")
+    test_string := "this is one word this is another word and another word"
+
+    topwords := NewTopwords(6)
+    var inserter = topwords.start.get_inserter()
+    for b := range(test_string) {
+        inserter(test_string[b])
+    }
+    inserter(' ') // ensure the last word is flushed
+
+    fmt.Printf("\ndump---------------\n")
+    topwords.start.dump()
+
+    topwords.most_frequent() 
+
+    fmt.Printf("Topwords returned %v words\n", len(topwords.sortedwords))
+
+    for t := range(topwords.sortedwords) {
+        fmt.Printf("%v %v\n", topwords.sortedwords[t].count, topwords.sortedwords[t].word)
+    }
+
+    }
+
+
+func TestSortedOutLong(t *testing.T) {
+
+    fmt.Println("\n--------------------------------TestSortedOutLong\n")
+    test_string := test_string()
+
+    topwords := NewTopwords(6)
+    var inserter = topwords.start.get_inserter()
+    for b := range(test_string) {
+        inserter(test_string[b])
+    }
+    inserter(' ') // ensure the last word is flushed
+
+    fmt.Printf("\ndump---------------\n")
+    topwords.start.dump()
+
+    topwords.most_frequent() 
+
+    fmt.Printf("Topwords returned %v words\n", len(topwords.sortedwords))
+
+    for t := range(topwords.sortedwords) {
+        fmt.Printf("%v %v\n", topwords.sortedwords[t].count, topwords.sortedwords[t].word)
+    }
+
+    }
